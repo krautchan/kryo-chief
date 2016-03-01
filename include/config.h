6d5f7345208d50c093e190189f4a96ecb00d43e2
Martin Wolters <wolters.mar@gmail.com>
2016-03-01 19:57:38 +0100

    config.h must override redefinitions in other places

--- include/config.h
+++ include/config.h
@@ -3,52 +3,22 @@
 
 /* Shared configuration */
 
-#ifndef CONFIG_PREALLOC_FILES
 #define CONFIG_PREALLOC_FILES	16
-#endif
-
-#ifndef CONFIG_RC4_DROP
 #define CONFIG_RC4_DROP			4096
-#endif
 
 /* Server configuration */
 
-#ifndef CONFIG_RSA_KSIZE
 #define CONFIG_RSA_KSIZE		1024
-#endif
-
-#ifndef CONFIG_PREGEN_KEYS
 #define CONFIG_PREGEN_KEYS		128
-#endif
-
-#ifndef CONFIG_REGEN_KEYS
 #define CONFIG_REGEN_KEYS		96
-#endif
-
-#ifndef CONFIG_KEYGEN_SLEEP
 #define CONFIG_KEYGEN_SLEEP		60
-#endif
-
-#ifndef CONFIG_KEYTAB_SIZE
 #define CONFIG_KEYTAB_SIZE		128
-#endif
-
-#ifndef CONFIG_KEYDIR
 #define CONFIG_KEYDIR			"etc/keystore"
-#endif
-
-#ifndef CONFIG_KEYFILE_EXT
 #define CONFIG_KEYFILE_EXT		".key"
-#endif
 
 /* Client configuration */
 
-#ifndef CONFIG_CRYPTED_EXT
 #define CONFIG_CRYPTED_EXT		".enc"
-#endif
-
-#ifndef CONFIG_CRYPTDIR
 #define CONFIG_CRYPTDIR			"local/share/Steam"
-#endif
 
 #endif
